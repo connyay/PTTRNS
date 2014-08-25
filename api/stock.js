96cff9f17d584ce432bd41e7e3afa71bfeaac2d1
Connor Hindley <conn.hindley@gmail.com>
2014-08-25 08:23:03 -0400

    add find by symbol method

--- api/stock.js
+++ api/stock.js
@@ -3,9 +3,16 @@ var StockPoint = require('mongoose').model('StockPoint');
 
 exports.index = function(req, res, next) {
     StockPoint.find()
-        .sort({
-            '_id': 1
-        })
+        .exec(function(err, stocks) {
+            if (err) {
+                return next(err);
+            }
+            res.json(stocks);
+        });
+};
+
+exports.findBySymbol = function(req, res, next) {
+    StockPoint.find({symbol: req.params.symbol})
         .exec(function(err, stocks) {
             if (err) {
                 return next(err);
--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/stocks', api.stock.index);
 
+app.get('/stocks/:symbol', api.stock.findBySymbol);
+
 // Start it up
 app.listen(app.get('port'), function() {
     console.log('Node app is listening on port: ' + app.get('port'));
